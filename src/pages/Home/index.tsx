27daa2e77de266dc813941104cb6693289871300
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-02-03 14:06:38 -0300

    feat: create page home to components encapsulate

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+// Components
+import { Text, RedirectButton } from "../../dto";
+
+export default function Home() {
+    
+    return (
+      <>
+        <Text
+          children={
+            "A Wecomp é a semana da computação do IFSP São Carlos, que neste ano sua edição. O objetivo do evento é proporcionar uma maior interação entre os estudantes, o mercado de tecnologia e a cidade de São Carlos. Ao longo de cinco dias de evento, serão realizados diversos minicursos, talks e processos seletivos, que visam reduzir a distância entre a teoria e a prática, ensinar tecnologias novas e promover o ingresso dos participantes no mercado de trabalho."
+          }
+        />
+        <RedirectButton
+          link={
+            "https://github.com/pet-ads/wecomp-new/blob/main/src/components/sections/Supporters/index.tsx"
+          }
+          children={"Lorem"}
+        />
+      </>
+    );
+}
